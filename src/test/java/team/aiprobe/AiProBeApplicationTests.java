73204c63032f442397e008f1e8544e71cf195b9b
5uhwann <5uhwann27@gmail.com>
2024-04-20 15:12:51 +0900

    init: init project

---
+++ src/test/java/team/aiprobe/AiProBeApplicationTests.java
@@ -0,0 +1,13 @@
+package team.aiprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
